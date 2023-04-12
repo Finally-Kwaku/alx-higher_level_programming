163e4185c5a829fd0e63839104bfbd6694d74212
Finally-Kwaku <kwakunagyamfi@gmail.com>
2023-04-11 18:00:56 -0700

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Creates a Mylist class"""
+
+
+class MyList(list):
+    """
+    A class MyList that inherits from list
+    """
+    def print_sorted(self):
+        """
+        Prints a sorted list
+        """
+        print(sorted(self))
