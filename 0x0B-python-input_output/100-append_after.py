82bde1970c9cc0a9ed4a9dcdd2c2a9f9bcc653ad
Finally-Kwaku <kwakunagyamfi@gmail.com>
2023-04-12 20:58:01 -0700

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file,
+    after each line containing a specific string
+    """
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    with open(filename, 'w') as f:
+        for line in lines:
+            if search_string in line:
+                f.write(line + new_string)
+            else:
+                f.write(line)
